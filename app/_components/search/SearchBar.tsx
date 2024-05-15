140f4341930ab785551d8edc370dcde31d2e614e
b3nk3lly <benjkelly98@gmail.com>
2024-05-15 19:13:21 -0300

    Make SearchIcon its own component

--- app/_components/search/SearchBar.tsx
+++ app/_components/search/SearchBar.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import Food from "../../types/food";
+import SearchIcon from "./SearchIcon";
 
 const SearchBar = (props: { onSelect: (food: Food) => void }) => {
 	const [query, setQuery] = useState("");
@@ -47,18 +48,7 @@ const SearchBar = (props: { onSelect: (food: Food) => void }) => {
 	return (
 		<>
 			<label className="input input-bordered flex items-center gap-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 16 16"
-					fill="currentColor"
-					className="w-4 h-4 opacity-70"
-				>
-					<path
-						fillRule="evenodd"
-						d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-						clipRule="evenodd"
-					/>
-				</svg>
+				<SearchIcon className="w-4 h-4 opacity-70" />
 				<input
 					type="text"
 					placeholder="Enter a name or food code"
