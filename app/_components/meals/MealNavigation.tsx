fb9fc47780794c98deb6a071426a6e5252717857
b3nk3lly <benjkelly98@gmail.com>
2024-05-15 21:19:14 -0300

    Increase navigation button size

--- app/_components/meals/MealNavigation.tsx
+++ app/_components/meals/MealNavigation.tsx
@@ -9,13 +9,13 @@ export default function MealNavigation(
 	return (
 		<div className="flex flex-wrap justify-center w-full py-2 gap-2">
 			{props.mealIds.map((id, index) => (
-				<a key={id} href={`#meal${id}`} className="btn btn-xs">
+				<a key={id} href={`#meal${id}`} className="btn btn-circle btn-sm">
 					{index + 1}
 				</a>
 			))}
 			<input
 				type="button"
-				className="btn btn-xs"
+				className="btn btn-circle btn-sm"
 				value="+"
 				onClick={() => props.onNewMeal()}
 			/>
